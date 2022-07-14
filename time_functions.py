969dc94227688f893d9483662b76d0d26b0b6a26
ruthlorenz <ruth.lorenz22@gmail.com>
2022-07-14 09:51:18 +0200

    use f-strings and other linting

--- time_functions.py
+++ time_functions.py
@@ -18,50 +18,59 @@ import xarray as xr
 
 
 def calc_1h_to_6h(varn, infile, sixhour_file):
-    # 6-hourly variables should all be "time: point"
-    ds = xr.open_dataset(infile)
-    var = ds[varn]
+    """
+    Function to resample data from hourly to 6-hourly
+    6-hourly variables cell_methods should all be "time: point"
+    """
+    ds_in = xr.open_dataset(infile)
+    var = ds_in[varn]
     if var.attrs['cell_methods'] == "time: point":
-        ds_6h = ds.resample(time='6h')
+        ds_6h = ds_in.resample(time='6h')
         ds_6h.to_netcdf(sixhour_file, format='NETCDF4_CLASSIC')
 
     else:
-        errormsg = ('Wrong cell_method, should be point but is %s'
-                    %(var.attrs['cell_methods']))
-        logger.error(errormsg)
-    ds.close()
+        errormsg = (f'Wrong cell_method, should be point but is '
+                    f'{var.attrs["cell_methods"]}')
+        logging.error(errormsg)
+    ds_in.close()
+
 
 def calc_to_day(varn, infile, day_file):
-    ds = xr.open_dataset(infile)
+    """
+    Function to calculate daily frequency VARIABLES
+    either as mean, maximum, or minimum depending on cell_methods
+    """
+
+    ds_in = xr.open_dataset(infile)
     if varn == 'snd':
         # snow depth should be mean over time
         # check cell_methods
-        snd = ds['snd']
+        snd = ds_in['snd']
         if snd.attrs['cell_methods'] == "time: mean":
-            ds_day = ds.resample(time='1D').mean()
+            ds_day = ds_in.resample(time='1D').mean()
         else:
-            errormsg = ('Wrong cell_method, should be mean but is %s'
-                %(snd.attrs['cell_methods']))
-            logger.error(errormsg)
+            errormsg = (f'Wrong cell_method, should be mean but is '
+                        f'{snd.attrs["cell_methods"]}')
+            logging.error(errormsg)
 
     if varn == 'tasmin':
         # daily minimum
-        tasmin = ds['tasmin']
+        tasmin = ds_in['tasmin']
         if tasmin.attrs['cell_methods'] == "time: minimum":
-            ds_day = ds.resample(time='1D').min()
-        else
-            errormsg = ('Wrong cell_method, should be minimum but is %s'
-                %(tasmin.attrs['cell_methods']))
-            logger.error(errormsg)
+            ds_day = ds_in.resample(time='1D').min()
+        else:
+            errormsg = (f'Wrong cell_method, should be minimum but is '
+                        f'{tasmin.attrs["cell_methods"]}')
+            logging.error(errormsg)
     if varn == 'tasmax':
         # daily maximum
-        tasmax = ds['tasmax']
+        tasmax = ds_in['tasmax']
         if tasmax.attrs['cell_methods'] == "time: maximum":
-            ds_day = ds.resample(time='1D').min()
-        else
-            errormsg = ('Wrong cell_method, should be maximum but is %s'
-                %(tasmax.attrs['cell_methods']))
-            logger.error(errormsg)
+            ds_day = ds_in.resample(time='1D').min()
+        else:
+            errormsg = (f'Wrong cell_method, should be maximum but is '
+                        f'{tasmax.attrs["cell_methods"]}')
+            logging.error(errormsg)
 
-    ds.to_netcdf(day_file, format='NETCDF4_CLASSIC')
-    ds.close()
+    ds_day.to_netcdf(day_file, format='NETCDF4_CLASSIC')
+    ds_in.close()
