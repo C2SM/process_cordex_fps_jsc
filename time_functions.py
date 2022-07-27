d813e4c58260f545d5d49dc3379ebf273035e095
ruthlorenz <ruth.lorenz22@gmail.com>
2022-07-27 14:11:35 +0200

    resample using mean() if cell_methods=time: mean, keep_attrs=True in resampling

--- time_functions.py
+++ time_functions.py
@@ -22,7 +22,8 @@ logger = logging.getLogger(__name__)
 def calc_1h_to_3h(varn, infile, threehour_file):
     """
     Function to resample data from hourly to 3-hourly
-    3-hourly variables cell_methods should all be "time: point"
+    3-hourly variables cell_methods should all be "time: point" or "time: mean"
+    Process as freq or mean, depending on cell_methods
 
     Parameters
     ----------
@@ -38,24 +39,36 @@ def calc_1h_to_3h(varn, infile, threehour_file):
     Nothing, netcdf written to disk
     """
     logger.info('Calculating 3-hourly values from hourly')
-    ds_in = xr.open_dataset(infile)
-    var = ds_in[varn]
-    if (var.attrs['cell_methods'] == "time: point" or
-        var.attrs['cell_methods'] == 'lev: mean'):
-        ds_3h = ds_in.resample(time='3H').asfreq()
-        ds_3h.to_netcdf(threehour_file, format='NETCDF4_CLASSIC')
+    with xr.open_dataset(infile) as ds_in:
+        try:
+            var = ds_in[varn]
+        except KeyError:
+            try:
+                new_key = re.split('(\d+)', varn)[0]
+                var = ds_in[new_key]
+                # ensure variable name is varn
+                ds_in.rename({new_key: varn})
+            except KeyError:
+                logger.error('The variable name in the file is not known')
+                logger.error(ds_in)
 
-    else:
-        errormsg = ('Wrong cell_method, should be time: point (or lev: mean)'
-                    f' but is {var.attrs["cell_methods"]}')
-        logger.error(errormsg)
-    ds_in.close()
-
+        if (var.attrs['cell_methods'] == 'time: point' or
+            var.attrs['cell_methods'] == 'lev: mean'):
+            ds_in.resample(time='3H', keep_attrs=True).asfreq()
+        elif (var.attrs['cell_methods'] == 'time: mean'):
+            ds_in.resample(time='3H', keep_attrs=True).mean()
+        else:
+            errormsg = ('Wrong cell_method, should be time: point (or lev: mean)'
+                        f' or time: mean but is {var.attrs["cell_methods"]}')
+            logger.error(errormsg)
+        ds_in.to_netcdf(threehour_file, format='NETCDF4_CLASSIC')
+        logger.info(f'3-hourly file {threehour_file} written.')
 
 def calc_1h_to_6h(varn, infile, sixhour_file):
     """
     Function to resample data from hourly to 6-hourly
-    6-hourly variables cell_methods should all be "time: point"
+    6-hourly variables cell_methods should all be "time: point" or "time: mean"
+    Process as freq or mean, depending on cell_methods
 
     Parameters
     ----------
@@ -86,19 +99,21 @@ def calc_1h_to_6h(varn, infile, sixhour_file):
 
         if (var.attrs['cell_methods'] == 'time: point' or
             var.attrs['cell_methods'] == 'lev: mean'):
-            ds_in.resample(time='6H').asfreq()
-            ds_in.to_netcdf(sixhour_file, format='NETCDF4_CLASSIC')
-            logger.info(f'6-hourly file {sixhour_file} written.')
+            ds_in.resample(time='6H', keep_attrs=True).asfreq()
+        elif (var.attrs['cell_methods'] == 'time: mean'):
+            ds_in.resample(time='6H', keep_attrs=True).mean()
         else:
             errormsg = ('Wrong cell_method, should be time: point (or lev: mean)'
-                        f' but is {var.attrs["cell_methods"]}')
+                        f' or time: mean but is {var.attrs["cell_methods"]}')
             logger.error(errormsg)
-
+        ds_in.to_netcdf(sixhour_file, format='NETCDF4_CLASSIC')
+        logger.info(f'6-hourly file {sixhour_file} written.')
 
 def calc_3h_to_6h(varn, infile, sixhour_file):
     """
     Function to resample data from 3-hourly to 6-hourly
-    6-hourly variables cell_methods should all be "time: point"
+    6-hourly variables cell_methods should all be "time: point" or "time: mean"
+    Process as freq or mean, depending on cell_methods
 
     Parameters
     ----------
@@ -129,14 +144,15 @@ def calc_3h_to_6h(varn, infile, sixhour_file):
 
         if (var.attrs['cell_methods'] == 'time: point' or
             var.attrs['cell_methods'] == 'lev: mean'):
-            ds_in.resample(time='6H').asfreq()
-            ds_in.to_netcdf(sixhour_file, format='NETCDF4_CLASSIC')
-            logger.info(f'6-hourly file {sixhour_file} written.')
+            ds_in.resample(time='6H', keep_attrs=True).asfreq()
+        elif (var.attrs['cell_methods'] == 'time: mean'):
+            ds_in.resample(time='6H', keep_attrs=True).mean()
         else:
             errormsg = ('Wrong cell_method, should be time: point (or lev: mean)'
-                        f' but is {var.attrs["cell_methods"]}')
+                        f' or time: mean but is {var.attrs["cell_methods"]}')
             logger.error(errormsg)
-
+        ds_in.to_netcdf(sixhour_file, format='NETCDF4_CLASSIC')
+        logger.info(f'6-hourly file {sixhour_file} written.')
 
 def calc_to_day(varn, infile, day_file):
     """
@@ -157,39 +173,38 @@ def calc_to_day(varn, infile, day_file):
     Nothing, netcdf written to disk
     """
     logger.info('Calculating daily values')
-    ds_in = xr.open_dataset(infile)
-    if varn == 'snd':
-        # snow depth should be mean over time
-        # check cell_methods
-        snd = ds_in['snd']
-        if snd.attrs['cell_methods'] == "time: mean":
-            ds_day = ds_in.resample(time='1D').mean()
+    with xr.open_dataset(infile) as ds_in:
+        if varn == 'snd':
+            # snow depth should be mean over time
+            # check cell_methods
+            snd = ds_in['snd']
+            if snd.attrs['cell_methods'] == "time: mean":
+                ds_in.resample(time='1D', keep_attrs=True).mean()
+            else:
+                errormsg = ('Wrong cell_method, should be mean but is '
+                            f'{snd.attrs["cell_methods"]}')
+                logger.error(errormsg)
+
+        elif varn == 'tasmin':
+            # daily minimum
+            tasmin = ds_in['tasmin']
+            if tasmin.attrs['cell_methods'] == "time: minimum":
+                ds_in.resample(time='1D', keep_attrs=True).min()
+            else:
+                errormsg = ('Wrong cell_method, should be minimum but is '
+                            f'{tasmin.attrs["cell_methods"]}')
+                logger.error(errormsg)
+        elif varn == 'tasmax':
+            # daily maximum
+            tasmax = ds_in['tasmax']
+            if tasmax.attrs['cell_methods'] == "time: maximum":
+                ds_in.resample(time='1D', keep_attrs=True).max()
+            else:
+                errormsg = ('Wrong cell_method, should be maximum but is '
+                            f'{tasmax.attrs["cell_methods"]}')
+                logger.error(errormsg)
         else:
-            errormsg = ('Wrong cell_method, should be mean but is '
-                        f'{snd.attrs["cell_methods"]}')
+            errormsg = ('Not implemented! variable needs to be snd, tasmax, or tasmin.')
             logger.error(errormsg)
 
-    elif varn == 'tasmin':
-        # daily minimum
-        tasmin = ds_in['tasmin']
-        if tasmin.attrs['cell_methods'] == "time: minimum":
-            ds_day = ds_in.resample(time='1D').min()
-        else:
-            errormsg = ('Wrong cell_method, should be minimum but is '
-                        f'{tasmin.attrs["cell_methods"]}')
-            logger.error(errormsg)
-    elif varn == 'tasmax':
-        # daily maximum
-        tasmax = ds_in['tasmax']
-        if tasmax.attrs['cell_methods'] == "time: maximum":
-            ds_day = ds_in.resample(time='1D').max()
-        else:
-            errormsg = ('Wrong cell_method, should be maximum but is '
-                        f'{tasmax.attrs["cell_methods"]}')
-            logger.error(errormsg)
-    else:
-        errormsg = ('Not implemented! variable needs to be snd, tasmax, or tasmin.')
-        logger.error(errormsg)
-
-    ds_day.to_netcdf(day_file, format='NETCDF4_CLASSIC')
-    ds_in.close()
+        ds_in.to_netcdf(day_file, format='NETCDF4_CLASSIC')
